4ebf201fd0b9f619c570333701980057cb75a42c
Yogev <finegoz@gmail.com>
2017-07-23 15:54:28 -0700

    Added image and name to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :update_sanitized_params, if: :devise_controller?
+
+	def update_sanitized_params
+	  devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:name, :image, :password, :email)}
+	end
 end
