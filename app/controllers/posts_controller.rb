cfe45a24c0c44cd995b36657d80b0c97070377cd
Yogev <finegoz@gmail.com>
2017-07-24 12:10:02 -0700

    fix index route for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,7 +31,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to action: "index", notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
+        format.json { render :index, status: :created, location: @post }
       else
         format.html { render :new }
         format.json { render json: @post.errors, status: :unprocessable_entity }
