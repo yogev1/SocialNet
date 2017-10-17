518914defb1ee9060e61b830403194029ba4f2f8
Yogev <finegoz@gmail.com>
2017-10-16 17:23:31 -0700

    more profile appearance

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :update_sanitized_params, if: :devise_controller?
+  before_action :get_time
+
+	def get_time
+		@time_now = Time.zone.now
+  end
 
 	def update_sanitized_params
 	  devise_parameter_sanitizer.permit(:sign_up) {|u| u.permit(:name, :image, :password, :email)}
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,8 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order(created_at: "DESC")
-    @time_now = Time.zone.now 
+    @posts = Post.all.order(created_at: "DESC") 
   end
 
   # GET /posts/1
