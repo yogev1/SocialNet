8935f7f8dec7eee0c0cc76bf6f9a4306f7aa4b2e
Yogev <finegoz@gmail.com>
2017-10-16 23:27:55 -0700

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :authenticate_user!
 
   before_action :update_sanitized_params, if: :devise_controller?
   before_action :get_time
