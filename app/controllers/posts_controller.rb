2e8f36f7c78d1466e5c6c0bc911fdd77e100c53d
Yogev <finegoz@gmail.com>
2017-10-18 20:25:39 -0700

    update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all.order(created_at: "DESC") 
+    @posts = Post.all.order(created_at: "DESC").limit(20) 
   end
 
   # GET /posts/1
